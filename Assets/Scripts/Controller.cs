1963070cfc2a9a261924faa1a653b2a390ac0ef9
Adam Rzadkowski <aorzadkowski@gmail.com>
2017-09-01 12:22:44 -0500

    Added basic plants and growth possibilities
    Plants grow based on nutrition values provided from the ground.

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public KeyCode[] forward { get; set; } 
+	public KeyCode[] backward { get; set; }
+	public KeyCode[] left { get; set; }
+	public KeyCode[] right { get; set; }
+	public KeyCode[] up { get; set; }
+	public KeyCode[] down { get; set; }
+    public KeyCode slow;
+    public KeyCode cameraLock;
+
+	public Camera mainCamera;
+
+	public static float thirdPersonDistance = 0;
+
+	public static bool flyingMode = true;
+    public static bool cameraKeyLock = true;
+
+	private static float movementScale = 0.3f;
+	private static float rotationScale = 5f;
+
+	private int xChunk = 0;
+	private int zChunk = 0;
+
+	// Use this for initialization
+	void Start () {
+		forward	= new KeyCode[]{ KeyCode.W, KeyCode.UpArrow };
+		backward = new KeyCode[]{ KeyCode.S, KeyCode.DownArrow };
+		left = new KeyCode[]{ KeyCode.A, KeyCode.LeftArrow };
+		right = new KeyCode[]{ KeyCode.D, KeyCode.RightArrow };
+		up = new KeyCode[]{ KeyCode.Q, KeyCode.Space };
+		down = new KeyCode[]{ KeyCode.E, KeyCode.LeftAlt };
+        slow = KeyCode.LeftShift;
+        cameraLock = KeyCode.Escape;
+
+		mainCamera = Camera.main;
+
+        Cursor.lockState = CursorLockMode.Locked;
+
+		/*
+		if (flyingMode)
+			movementScale = 2.5f;
+		else
+			movementScale = 0.2f;*/
+	}
+
+	bool keycodePressed(KeyCode[] arr) {
+		for(int i = 0; i < arr.Length; i++) {
+			if(Input.GetKey(arr[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool keycodeDown(KeyCode[] arr) {
+		for(int i = 0; i < arr.Length; i++) {
+			if(Input.GetKeyDown(arr[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 oldPosition = this.gameObject.transform.position;
+		Quaternion oldRotation = mainCamera.transform.rotation;
+
+		Vector3 newPosition = oldPosition;
+		Quaternion newRotation = oldRotation;
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		float planeRotation = mainCamera.transform.rotation.eulerAngles.y;
+		Quaternion quat = Quaternion.Euler (new Vector3 (0, planeRotation, 0));
+
+		Quaternion ang = Quaternion.identity;
+
+		bool onGround = false;
+
+        float newMovementScale = movementScale;
+
+        if (Input.GetKeyUp(cameraLock))
+        {
+            cameraKeyLock = !cameraKeyLock;
+            Debug.Log("Smashed that mfing escape key dawg.");
+        }
+        if (Input.GetKey(slow))
+        {
+            newMovementScale *= 0.5f;
+        }
+		if (keycodePressed (forward)) {
+			newPosition += (ang * quat * Vector3.forward * newMovementScale);
+		}
+		if (keycodePressed (backward)) {
+			newPosition += (ang * quat * Vector3.back * newMovementScale);
+		}
+		if (keycodePressed (left)) {
+			newPosition += (ang * quat * Vector3.left * newMovementScale);
+		}
+		if (keycodePressed (right)) {
+			newPosition += (ang * quat * Vector3.right * newMovementScale);
+		}
+
+        if (cameraKeyLock)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        } else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+		if (flyingMode) {
+			if (keycodePressed (up)) {
+				newPosition += (Vector3.up * movementScale);
+			}
+		} else {
+			//Only jump if we're on the ground (or very close)
+			if (keycodeDown (up)) {	
+				if (onGround) {
+					this.gameObject.GetComponent<Rigidbody> ().velocity = new Vector3 (0, 5, 0);
+				}
+			}
+		}
+			
+		if (flyingMode) {
+			if (keycodePressed (down)) {
+				newPosition += (Vector3.down * movementScale);
+			}
+		}
+
+		float mouseX = Input.GetAxis ("Mouse X");
+		float mouseY = -Input.GetAxis ("Mouse Y");
+		if (mouseX != 0 || mouseY != 0) {
+			Vector3 rot = oldRotation.eulerAngles;
+
+			float xRot = rot.x;
+
+			float tent = rot.x + (rotationScale * mouseY);
+
+			if (xRot > 270 && tent < 270) {
+				xRot = -89;
+			} else if (xRot < 90 && tent > 90) {
+				xRot = 89;
+			} else {
+				xRot += rotationScale * mouseY;
+			}
+            
+
+            float newYRot = rot.y + (rotationScale * mouseX);
+
+            newRotation = Quaternion.Euler (
+				new Vector3 (
+					xRot,
+					newYRot,
+					0));
+		}
+
+		thirdPersonDistance -= Input.GetAxis ("Mouse ScrollWheel");
+		if (thirdPersonDistance < 0)
+			thirdPersonDistance = 0;
+
+		this.gameObject.transform.position = newPosition;
+		this.gameObject.transform.rotation = ang;
+
+		mainCamera.transform.position = newPosition + (newRotation * new Vector3 (0, 0, -thirdPersonDistance));
+		mainCamera.transform.rotation = newRotation;
+
+
+
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity))
+            {
+                GameObject plant = hit.collider.gameObject;
+
+                if (plant != null && plant.GetComponent<PlantBehavior>() != null)
+                {
+                    Debug.Log("Growth Rate: " + plant.GetComponent<PlantBehavior>().growthRate + " Growth Limit: " + plant.GetComponent<PlantBehavior>().growthLimit);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlantBehavior.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantBehavior : MonoBehaviour {
+    float growth  = 0.0f;
+    public float growthRate = 0.0f;
+    public float growthLimit = 1.0f;
+    float maxGrowthRate = 0.001f;
+    float minGrowthRate = 0.00001f;
+
+    float maxHeight = 1;
+
+    float startingX;
+    float startingZ;
+    float startingY;
+
+    float idealNutritionValue;  // 0.0 - 1.0, anywhere between.
+    float nutritionRange;       // how far in one direction does the growable range extend to.
+
+    int framesAlive;
+
+	// Use this for initialization
+	void Start () {
+        startingX = transform.localPosition.x;
+        startingY = transform.localPosition.y;
+        startingZ = transform.localPosition.z;
+
+        framesAlive = 0;
+	}
+
+    float getGrowthRate(float nutritionValue)
+    {
+        float lowestBound = (idealNutritionValue - nutritionRange < 0 ? 0 : idealNutritionValue - nutritionRange);
+        float highestBound = (idealNutritionValue + nutritionRange > 1 ? 1 : idealNutritionValue + nutritionRange);
+
+        if (nutritionValue < lowestBound || nutritionValue > highestBound) return 0.0f;
+        else
+        {
+            float percentageDistace = (nutritionValue - nutritionRange) / (idealNutritionValue - nutritionRange);
+
+            return (maxGrowthRate - minGrowthRate) * percentageDistace + minGrowthRate;
+        }
+    }
+
+    public void setNutritionValues(float inv, float nr)
+    {
+        if (inv < 0) idealNutritionValue = 0;
+        else if (inv > 1) idealNutritionValue = 1;
+        else idealNutritionValue = inv;
+
+        if (nr < 0) nutritionRange = 0;
+        else if (nr > 1) nutritionRange = 1;
+        else nutritionRange = nr;
+    }
+
+    public void setGrowthLimit(float gl)
+    {
+        growthLimit = gl;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        growthRate = getGrowthRate(0.0f);
+
+        transform.localScale = new Vector3(0.3f, maxHeight * growth, 0.3f) ;
+        transform.localPosition = new Vector3(startingX, startingY + growth, startingZ);
+
+        Component collider = GetComponent<CapsuleCollider>();
+        collider.transform.localScale = new Vector3(0.3f, maxHeight * growth, 0.3f);
+        collider.transform.localPosition = new Vector3(startingX, startingY + growth, startingZ);
+
+        growth += growthRate;
+        growth %= growthLimit;
+
+        framesAlive++;
+	}
+}
